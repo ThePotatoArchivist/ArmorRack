d0f5cb27997246cc94837c9d8f54da9ed7d7e9bd
JosiahFu <josiahfu@gmail.com>
2024-12-07 19:36:41 -0800

    Add rack rendering cache

--- src/client/java/archives/tater/armorrack/ArmorRackItemRenderer.java
+++ src/client/java/archives/tater/armorrack/ArmorRackItemRenderer.java
@@ -6,33 +6,30 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import java.util.Map;
+import java.util.WeakHashMap;
 
 public class ArmorRackItemRenderer {
 
-//    private static @Nullable ArmorRackEntity RENDERED_ARMOR_RACK = null;
-//
-//    private static final NbtCompound EMPTY = Util.make(() -> {
-//        var compound = new NbtCompound();
-//        var armorItems = new NbtList();
-//        for (int i = 0; i < 4; i++) {
-//            armorItems.add(new NbtCompound());
-//        }
-//        var handItems = new NbtList();
-//        for (int i = 0; i < 2; i++) {
-//            handItems.add(new NbtCompound());
-//        }
-//        compound.put("ArmorItems", armorItems);
-//        compound.put("HandItems", handItems);
-//        return compound;
-//    });
+    private static final Map<ItemStack, ArmorRackEntity> CACHE = new WeakHashMap<>();
+
+    private static ArmorRackEntity createRack(ItemStack itemStack, World world) {
+        var entity =  new ArmorRackEntity(ArmorRack.ARMOR_RACK_ENTITY, world);
+        var nbt = itemStack.getSubNbt("EntityTag");
+        if (nbt != null) entity.readNbt(nbt);
+        return entity;
+    }
+
+    private static ArmorRackEntity getOrCreate(ItemStack itemStack, World world) {
+        return CACHE.computeIfAbsent(itemStack, (_stack) -> createRack(itemStack, world));
+    }
 
     public static void renderArmorRack(ItemStack itemStack, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         var world = MinecraftClient.getInstance().world;
         if (world == null)
             MinecraftClient.getInstance().getItemRenderer().renderItem(itemStack, ModelTransformationMode.NONE, false, matrices, vertexConsumers, light, overlay, MinecraftClient.getInstance().getBakedModelManager().getModel(ArmorRack.FALLBACK_MODEL_ID));
-        var entity = new ArmorRackEntity(ArmorRack.ARMOR_RACK_ENTITY, world);
-        var nbt = itemStack.getSubNbt("EntityTag");
-        if (nbt != null) entity.readNbt(nbt);
-        MinecraftClient.getInstance().getEntityRenderDispatcher().render(entity, 0, 0, 0, 0, 0, matrices, vertexConsumers, light);
+        MinecraftClient.getInstance().getEntityRenderDispatcher().render(getOrCreate(itemStack, world), 0, 0, 0, 0, 0, matrices, vertexConsumers, light);
     }
 }
